816ba4194b5b4bf7b674401c8238f73817eed868
Pranav Ladole <pranavladole98@gmail.com>
2022-09-21 00:30:48 +0530

    code added

---
+++ src/main/java/com/example/test/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.example.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	
+	@GetMapping("/nci")
+	public String test() {
+		return "tested nci";
+	}
+
+	
+}
